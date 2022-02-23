6d8f53d854b31bb1b738d41e608e3bfa549cb180
benchan777 <benchan777@gmail.com>
2022-02-22 22:50:43 -0800

    changed what items are returned from scraper

--- backend/src/routes/menuData.js
+++ backend/src/routes/menuData.js
@@ -5,11 +5,11 @@ const { menuScraper } = require('../controllers/scraper');
 router.get('/scraper', (req, res) => {
   menuScraper()
     .then(result => {
-      // const menuItems = result['menus'][0]['menuItems'];
+      const menuItems = result['menus'][0]['menuItems'];
       // Object.keys(menuItems).forEach(key => {
       //   console.log(menuItems[key]['name_for_customer']);
       // });
-      res.json(result);
+      res.json(menuItems);
     })
     .catch(error => {
       res.json({ Error: error });
