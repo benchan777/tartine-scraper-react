10cc631e67e6ffbd677ff73596c6c656b721893b
benchan777 <benchan777@gmail.com>
2022-02-22 17:25:47 -0800

    functional scraper & return data to api endpoint

--- backend/src/controllers/scraper.js
+++ backend/src/controllers/scraper.js
@@ -1,5 +1,21 @@
+require('dotenv').config();
+const axios = require('axios');
+const url = 'https://api.guerrero.tartine.menu/api/cached/menu-data?host=guerrero.tartine.menu&ids[]=840a9600-8e13-47cb-b08b-85db705c6e51';
+
 const menuScraper = () => {
-  console.log('scraper working');
-}
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'get',
+      url: url,
+    })
+      .then(response => {
+        const data = response.data
+        resolve(data);
+      })
+      .catch(error => {
+        reject(`Error at axios: ${error.message}`);
+      });
+  });
+};
 
 module.exports = { menuScraper };
--- backend/src/routes/menuData.js
+++ backend/src/routes/menuData.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { menuScraper } = require('../controllers/scraper')
+const { menuScraper } = require('../controllers/scraper');
 
 router.get('/scraper', (req, res) => {
-  menuScraper();
-  console.log('Test');
+  menuScraper()
+    .then(result => {
+      // const menuItems = result['menus'][0]['menuItems'];
+      // Object.keys(menuItems).forEach(key => {
+      //   console.log(menuItems[key]['name_for_customer']);
+      // });
+      res.json(result);
+    })
+    .catch(error => {
+      res.json({ Error: error });
+    });
 });
 
 module.exports = router;
