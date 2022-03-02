6a87b42e93217d61db5e7fee337c84ca1fcb6bf2
Jackie <taekwondo.chan@gmail.com>
2022-03-02 12:56:24 -0800

    add footer & styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Title from './Title/Title';
+import Footer from './Footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <Title />
-      <Outlet />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Title />
+			<Outlet />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<div className="Footer">
+			<p>Copyright Ben Chan 2022</p>
+		</div>
+	);
+}
+
+export default Footer;
